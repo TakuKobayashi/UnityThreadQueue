a5d90e9af3b4ed30762c17d857bd2425d370dda5
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-07-23 16:13:59 +0900

    Add Library and Sample Script

---
+++ Assets/Plugins/UnityThreadQueue/UnityThreadQueue.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+public class UnityThreadQueue : IDisposable{
+	private static UnityThreadQueue instance;
+
+	public static UnityThreadQueue Instance{
+		get
+		{
+			if (instance == null){
+				instance = new UnityThreadQueue();
+			}
+			return instance;
+		}
+	}
+
+	private UnityThreadQueue(){
+		ThreadPool.QueueUserWorkItem(new WaitCallback(Execute));
+	}
+
+	private Queue<Action> taskQueue = new Queue<Action>();
+	private volatile bool isExistQueueEvent = false;
+	private volatile bool running = true;
+	private ManualResetEvent queueEvent = new ManualResetEvent(false);
+
+	private void Execute(object o){
+		while (queueEvent.WaitOne()){
+			if (!running) { break; }
+
+			Action actor = null;
+			lock (taskQueue){
+				if (taskQueue.Count > 0){
+					actor = taskQueue.Dequeue();
+				}else{
+					isExistQueueEvent = false;
+					queueEvent.Reset();
+				}
+			}
+			if (actor != null){
+				try{
+					actor();
+				}catch (Exception e){
+					lock (taskQueue) {
+						running = false;
+						taskQueue.Clear ();
+					}
+					throw;
+				}
+			}
+		}
+	}
+
+	public bool ExistQueueEvent{
+		get{
+			return isExistQueueEvent;
+		}
+	}
+
+	public void Enqueue(Action task)
+	{
+		lock (taskQueue)
+		{
+			taskQueue.Enqueue(task);
+			queueEvent.Set();
+			isExistQueueEvent = true;
+		}
+	}
+
+	public void Dispose(){
+		running = false;
+		isExistQueueEvent = false;
+		queueEvent.Set();
+	}
+}
---
+++ Assets/Scripts/AsyncSample.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class AsyncSample : MonoBehaviour {
+	private int counter = 0;
+
+	void Start () {
+		// first task
+		UnityThreadQueue.Instance.Enqueue (() => {
+			for(int i = 0;i < 10;++i){
+				Thread.Sleep(1000);
+				counter++;
+			}
+		});
+		// second task
+		UnityThreadQueue.Instance.Enqueue (() => {
+			counter = -129389152;
+			Thread.Sleep(3000);
+		});
+	}
+	
+	void OnGUI() {
+		if (UnityThreadQueue.Instance.ExistQueueEvent) {
+			GUI.Label (new Rect (300, 0, 200, 200), counter.ToString ());
+		} else {
+			GUI.Label (new Rect (300, 0, 200, 200), "complete!!!!");
+		}
+	}
+}
