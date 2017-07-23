c645061c3e4f4a1ce899cf8377b0d2c0bc40b97c
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-07-23 16:43:32 +0900

    add sync sample

---
+++ Assets/Scripts/SyncSample.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SyncSample : MonoBehaviour {
+	[SerializeField] private Text counterText;
+	private bool complete = false;
+
+	void Start () {
+		// first task
+		UnityThreadQueue.Instance.Enqueue (() => {
+			for(int i = 0;i < 10;++i){
+				Thread.Sleep(1000);
+			}
+			complete = true;
+		});
+		StartCoroutine (SyncCorutine ());
+	}
+
+	private IEnumerator SyncCorutine(){
+		while (UnityThreadQueue.Instance.ExistQueueEvent && !complete) {
+			yield return null;
+		}
+		counterText.text = "complete";
+	}
+}
